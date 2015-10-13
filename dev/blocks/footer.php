b9cf988506f3e2745fc2c52f995f54229efee1a4
ilcomp <cgde88@gmail.com>
2015-10-13 12:02:53 +0300

    u

--- dev/blocks/footer.php
+++ dev/blocks/footer.php
@@ -39,7 +39,7 @@
 	    <div class="col-md-6">
 	    	<span>© 2013-2015 <span itemprop="name">ЗАО «СМП-245-ТРАНССТРОЙ»</span></span>
 	    </div>
-	    <div class="col-xs-8 col-md-4"><p><a href="http://www.creative-seo.ru/" class="white-a">Создание сайтов</a></p></div>
+	    <div class="col-xs-8 col-md-4"><p><a href="http://www.creative-seo.ru/" class="white-a">Создание сайта</a></p></div>
 	    <div class="col-xs-4 col-md-2"><div class="pull-right">
 	    	<!-- Yandex.Metrika informer --><a href="https://metrika.yandex.ru/stat/?id=30537562&amp;from=informer" target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/30537562/3_1_FFFFFFFF_EFEFEFFF_0_pageviews" style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:30537562,lang:'ru'});return false}catch(e){}"/></a><!-- /Yandex.Metrika informer --><!-- Yandex.Metrika counter -->
 	    	<script type="text/javascript">(function(d,w,c){(w[c]=w[c]||[]).push(function(){try{w.yaCounter30537562=new Ya.Metrika({id:30537562,webvisor:true,clickmap:true,trackLinks:true,accurateTrackBounce:true});}catch(e){}});var n=d.getElementsByTagName("script")[0],s=d.createElement("script"),f=function(){n.parentNode.insertBefore(s,n);};s.type="text/javascript";s.async=true;s.src=(d.location.protocol=="https:"?"https:":"http:")+"//mc.yandex.ru/metrika/watch.js";if(w.opera=="[object Opera]"){d.addEventListener("DOMContentLoaded",f,false);}else{f();}})(document,window,"yandex_metrika_callbacks");</script>
